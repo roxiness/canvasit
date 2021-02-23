66b8fc66c0c1f544318bb5756d8291a99c23ce82
Jakob Rosenberg <jakobrosenberg@gmail.com>
2021-02-23 21:41:22 +0100

    test: placeholder parts should be strings

--- test/realWorld/fragments/routify/template/src/App.svelte.fragment.js
+++ test/realWorld/fragments/routify/template/src/App.svelte.fragment.js
@@ -1,11 +1,12 @@
 module.exports.patch = ({ placeholders, configs, stringify }) => { 
     placeholders.constants.append('configs')
 
-    placeholders.imports.push`
+    placeholders.imports.push(`
         import { Router } from "@roxi/routify";
         import { routes } from "../.routify/routes";
-    `
+    `)
+
     placeholders.configs.push(`const config = ${(stringify(configs.routify.runtime))}`)
 
-    placeholders.html.push`<Router { config } { routes } />`
+    placeholders.html.push(`<Router { config } { routes } />`)
 }
